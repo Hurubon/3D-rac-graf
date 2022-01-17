a993e31b6fd0c9011126f84d850b536b451d8b8b
hrvoje-zohar <hrvoje.zohar@roxoft.hr>
2022-01-17 21:11:10 +0100

    Finished.

--- zadaca_5/main.cpp
+++ zadaca_5/main.cpp
@@ -39,7 +39,9 @@ class BezierCurve
 
         std::vector<glm::vec2> GetCurve(int const granularity) const noexcept
         {
-            auto points = std::vector<glm::vec2>(granularity);
+            auto points = std::vector<glm::vec2>{};
+
+            points.reserve(granularity);
 
             for (float t = 0; t <= 1; t += 1.f / granularity)
             {
@@ -49,13 +51,39 @@ class BezierCurve
             return std::move(points);
         }
 
-        glm::vec2 Interpolate(
-        	float const t)
-        const noexcept
+        glm::vec2 Interpolate(float const t) const noexcept
         {
         	return b0(t) * P0 + b1(t) * P1 + b2(t) * P2 + b3(t) * P3;
         }
 };
+
+auto curve = BezierCurve({100, 100}, {100, 500}, {900, 100}, {900, 500});
+auto* held = static_cast<glm::vec2*>(nullptr);
+
+static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (held)
+        *held = {xpos, ypos};
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    double x, y;
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        glfwGetCursorPos(window, &x, &y);
+        if      ( glm::length(curve.P0 - glm::vec2{x, y}) < 5 ){ held = &curve.P0; }
+        else if ( glm::length(curve.P1 - glm::vec2{x, y}) < 5 ){ held = &curve.P1; }
+        else if ( glm::length(curve.P2 - glm::vec2{x, y}) < 5 ){ held = &curve.P2; }
+        else if ( glm::length(curve.P3 - glm::vec2{x, y}) < 5 ){ held = &curve.P3; }
+        else
+            held = nullptr;
+    }
+    else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+    {
+        held = nullptr;
+    }
+}
 
 int main ()
 {
@@ -87,11 +115,13 @@ int main ()
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
-    auto const curve = BezierCurve({100, 100}, {100, 500}, {900, 100}, {900, 500});
-    auto points = curve.GetCurve(1024);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
 
     while (!glfwWindowShouldClose(window))
     {
+        auto points = curve.GetCurve(1024);
+
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -101,7 +131,7 @@ int main ()
         RenderCircle(curve.P2, 5);
         RenderCircle(curve.P3, 5);
 
-        glfwPollEvents();
+        glfwWaitEvents();
         glfwSwapBuffers(window);
     }
 
